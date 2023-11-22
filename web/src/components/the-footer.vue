bdf035d439d61be08fd49cb3068079bd705bf247
Lenovo <316125197@qq.com>
2023-11-22 21:01:22 +0800

    4-7 开发网站首页布局，完成the-header;the-footer单独提取

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    Wiki Design ©2023 Created by XiDian Guangzhou
+  </a-layout-footer>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+
+
+  export default defineComponent({
+    name: 'the-footer',
+
+  });
+</script>
